22a858bcb5cb20ffcc09dff6774b3fe26b2bc890
Arghya Chanda <arghyac35@gmail.com>
2020-09-24 20:00:18 +0530

    Fix /raw for short id
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const rawBody = require('./middleware/rawBody');
-const mongoose = require('./middleware/mongoose');
-const render = require('./middleware/render');
-const isHash = require('./middleware/isHash');
+const rawBody = require("./middleware/rawBody");
+const mongoose = require("./middleware/mongoose");
+const render = require("./middleware/render");
+const isHash = require("./middleware/isHash");
 
 const app = express();
 
-const home = require('./routes/home');
-const getSnippet = require('./routes/getSnippet');
-const getSnippetRaw = require('./routes/getSnippetRaw');
-const createSnippet = require('./routes/createSnippet');
-const fork = require('./routes/fork');
+const home = require("./routes/home");
+const getSnippet = require("./routes/getSnippet");
+const getSnippetRaw = require("./routes/getSnippetRaw");
+const createSnippet = require("./routes/createSnippet");
+const fork = require("./routes/fork");
 
 app.use(helmet());
-app.use(express.static(__dirname + '/public'));
-app.use('/custom', express.static(__dirname + '/custom'));
-app.use(express.urlencoded({extended: true})); // parse formdata
+app.use(express.static(__dirname + "/public"));
+app.use("/custom", express.static(__dirname + "/custom"));
+app.use(express.urlencoded({ extended: true })); // parse formdata
 app.use(express.json()); // parse JSON requests
 app.use(rawBody());
 
 app.use(mongoose());
 app.use(render());
 
-app.post('/fork', fork);
-app.post('/', createSnippet);
+app.post("/fork", fork);
+app.post("/", createSnippet);
 
-app.get('/~:id', getSnippet);
-app.get('/!:hash', isHash, getSnippet);
-app.get('/~:id/raw', getSnippetRaw);
-app.get('/', home);
-
+app.get("/~:id", getSnippet);
+app.get("/!:hash", isHash, getSnippet);
+app.get("/!:hash/raw", isHash, getSnippetRaw);
+app.get("/~:id/raw", getSnippetRaw);
+app.get("/", home);
 
 const PORT = process.env.PORT || 1998;
-app.listen(PORT, () => console.log('Listening on port', PORT));
+app.listen(PORT, () => console.log("Listening on port", PORT));
--- routes/getSnippetRaw.js
+++ routes/getSnippetRaw.js
@@ -1,18 +1,19 @@
-const render = require('../render');
+const render = require("../render");
 
 module.exports = (req, res) => {
-    const {models, mongoose} = req;
-    const id = req.params.id;
-    const contentType = req.query.ctype || "text/plain";
-    return models.snippets.findOne({_id: mongoose.Types.ObjectId(id)})
-        .then(doc => {
-            if (!doc) {
-                res.status(404);
-                return res.send(
-                    '<code>Nothing here, go <a href="/">back!</a></code>'
-                );
-            }
-            res.writeHead(200, {"Content-Type": contentType + "; charset=utf-8"});
-            res.end(doc.content);
-        });
+  const { models, isHash } = req;
+  const id = isHash ? req.params.hash : req.params.id;
+  const contentType = req.query.ctype || "text/plain";
+
+  return (isHash
+    ? models.snippets.findOne({ hash: id })
+    : models.snippets.findById(id)
+  ).then((doc) => {
+    if (!doc) {
+      res.status(404);
+      return res.send('<code>Nothing here, go <a href="/">back!</a></code>');
+    }
+    res.writeHead(200, { "Content-Type": contentType + "; charset=utf-8" });
+    res.end(doc.content);
+  });
 };
